224345d7a53eab5c84ee4b647455dea4672e1a60
Mesut <yildirimmesutyigit@gmail.com>
2023-05-08 22:24:12 +0300

    new commit

--- javascript/class-notes/js03-conditions/conditions.js
+++ javascript/class-notes/js03-conditions/conditions.js
@@ -4,8 +4,8 @@
 
 console.log("****** CONDITIONS *******");
 
-const calismaSuresi = prompt("Calisma süresi:");
-let maas = prompt("Maasini giriniz:");
+// const calismaSuresi = prompt("Calisma süresi:")
+// let maas = prompt("Maasini giriniz:")
 
 //? 2 kosullu
 // if (calismaSuresi >= 10) {
@@ -19,25 +19,72 @@ let maas = prompt("Maasini giriniz:");
 // console.log("Güle güle")
 
 //? Tek kosullu
-if (calismaSuresi >= 10) {
-  maas = Math.round(maas * 1.1);
-  console.log(`Zamli maasiniz: ${maas}`);
-}
-console.log("Güle güle");
+// if (calismaSuresi >= 10) {
+//   maas = Math.round(maas * 1.1)
+//   console.log(`Zamli maasiniz: ${maas}`)
+// }
+// console.log("Güle güle")
 
 //? Cok kosullu
+const grade = prompt("Please enter your grade:");
 
-const grade = prompt(`Please enter your grade: `);
-let result = "";
-if (grade < 40) {
+let result = null;
+// let result = ""
+if (grade < 0) {
+  console.log("Grade can not be smaller than zero");
+} else if (grade < 30) {
   result = "FF";
 } else if (grade <= 50) {
   result = "DD";
-} else if (grade <= 60) {
-  result = "CC";
 } else if (grade <= 70) {
+  result = "CC";
+} else if (grade <= 90) {
   result = "BB";
-} else {
+} else if (grade <= 100) {
   result = "AA";
+} else {
+  console.log("Grade can not be bigger than 100");
 }
-console.log(`Your grade is ${result}`);
+
+if (result != null) {
+  console.log(`Your score:${result}`);
+}
+//? Kısa yol
+if (result) {
+  console.log(`Your score:${result}`);
+}
+
+//? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+//? yazdiriniz.
+
+const n1 = +prompt("Num1:");
+const n2 = +prompt("Num2:");
+const n3 = +prompt("Num3:");
+
+//* 1.YONTEM
+//? nested if
+// if (n1 > n2) {
+//   if (n1 > n3) {
+//     console.log("Biggest:", n1)
+//   }
+// }
+
+//? kosullarin logical operatorler yardimiyla birleştirilmesi
+if (n1 > n2 && n1 > n3) {
+  console.log("Biggest:", n1);
+} else if (n2 > n1 && n2 > n3) {
+  console.log("Biggest:", n2);
+} else if (n3 > n1 && n3 > n2) {
+  console.log("Biggest:", n3);
+}
+
+//* 2.YONTEM
+let biggest = n1;
+
+if (n2 >= biggest) {
+  biggest = n2;
+}
+if (n3 >= biggest) {
+  biggest = n3;
+}
+console.log(`${biggest} is biggest`);
