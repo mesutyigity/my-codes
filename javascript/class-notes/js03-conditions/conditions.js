340bafc792908fa0f5be455d834f4ff647275b45
Mesut <yildirimmesutyigit@gmail.com>
2023-05-08 21:02:19 +0300

    new commit

--- javascript/class-notes/js03-conditions/conditions.js
+++ javascript/class-notes/js03-conditions/conditions.js
@@ -2,4 +2,14 @@
 // *                  KARAR YAPILARI
 // * ===============================================
 
-console.log("****** CONDITIONS *******")
+console.log("****** CONDITIONS *******");
+
+const calismaSuresi = prompt("Calisma surenizi giriniz: ");
+let maas = prompt("Maasinizi giriniz: ");
+
+if (calismaSuresi >= 10) {
+  maas = maas * 1.1;
+  console.log('Zamli maasiniz: ${maas}')
+} else {45
+  console.log("Uzgunuz maasiniz zam yapilmayacaktir.");
+}
