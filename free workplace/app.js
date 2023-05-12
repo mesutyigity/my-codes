e5adc663ecf822452236ba343f42240559629b18
Mesut <yildirimmesutyigit@gmail.com>
2023-05-13 00:02:58 +0300

    new commit

---
+++ free workplace/app.js
@@ -0,0 +1,10 @@
+const kacinciSira = +prompt("Bir deger giriniz.");
+
+let n1 = 0,
+  n2 = 1,
+  n3;
+for (let i = 1; i <= kacinciSira; i++) {
+  console.log(n1);
+  n3 = n1 + n2;
+  console.log(n1, n2, n3);2
+}
