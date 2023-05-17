2e87fc70bc3bfffb00d35fc5b20ef2f9c9edcfab
Mesut <yildirimmesutyigit@gmail.com>
2023-05-17 23:03:53 +0300

    new commit

--- javascript/class-notes/js07-strings/App.js
+++ javascript/class-notes/js07-strings/App.js
@@ -131,3 +131,99 @@
 // console.log(str.lastIndexOf("O", 6));// -1
 
 // console.log(str.indexOf("o"));// 4
+
+//search()
+
+// let str = 'You do not KNOW what you do not know until you know.';
+
+// console.log (str.search("know"));  // 32
+
+// console.log (str.search(/know/)); // 32 with regular expression
+
+// console.log (str.search(/know/i)); // 11 (i means not case sensitive)
+// console.log (str.search(/klow/i)); // -1 içerisinde yoksa -1 döner
+
+// let str1 = 'hello World';
+// const arama = /[A-Z]/; // A-Z arası bütün büyük harfler
+// const arama2 = /[a-z]/; // A-Z arası bütün büyük harfler
+
+// console.log(str1.search(arama))
+
+//replace
+
+// let str = "Mr Brown has a brown house and a brown car";
+
+// let newStr = str.replace("brown", "red");
+// let newStr2 = str.replace(/brown/i, "red"); // casesensitivite ignore eder
+// let newStr3 = str.replace(/brown/ig, "red"); // ne kadar varsa değiştirir.
+// let newStr4 = str.replace(/brown/g, "red"); // küçük brownların tümünü değiştirir.
+
+// console.log(newStr);
+// console.log(newStr2);
+// console.log(newStr3);
+// console.log(newStr4);
+
+//replaceAll
+
+// let str = "Mr Brown has a brown house and a brown car";
+
+// console.log(str.replaceAll('Brown', 'red')) // sorguyu uyan tüm sonuçları değiştirir.
+
+//split stringi array e çevirir.
+
+// let str = "Welcome to: Full Stack";
+
+// // console.log(str.split())
+// // console.log(str.split(' '))
+// // console.log(str.split(':'))
+// // console.log(str.split('Full'))
+// // console.log(str.split('full'))
+
+// let arr = str.split(' ')
+
+// console.log(arr)
+// console.log(str)
+
+// toLowercase & toUpperCase
+
+// let str = "Welcome to Full Stack";
+// let str = "ĞŞİüöÇççççüüğğşşiiiööçç";
+// console.log(str.toLowerCase());
+// console.log(str.toLocaleLowerCase())
+
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleUpperCase('TR'));
+
+//trim
+
+// let str = "     Welcome to Full Stack    ";
+// console.log(str)
+// console.log(str.trim()) // Hem balştaki hem sondaki boşlukları temizler
+// console.log(str.trimStart()) // Baştaki boşlukları temizler
+// console.log(str.trimEnd()) // sondaki boşlukları temizler
+
+// startsWith & endWith
+
+// let str = "clarusway@Clarusway.com is our e-mail address";
+
+// console.log(str.startsWith("clarusway"))
+// console.log(str.startsWith("Clarusway"))
+// console.log(str.startsWith("Clarusway",10))
+
+// let email = "clarusway@Clarusway.com"
+
+// console.log(email.endsWith('com'))
+
+// chaining
+
+let str = "         Clarusway Full Stack";
+
+let str1 = str.trim(); // Clarusway Full Stack
+
+let str2 = str1.slice(0, 9); // Clarusway
+
+let str3 = str2.toUpperCase(); // CLARUSWAY
+
+console.log(str3); // CLARUSWAY
+
+console.log(str.trim().slice(0, 9).toUpperCase());
