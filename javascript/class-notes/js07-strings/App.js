5a20bd5e1f62f58234e59fa577e99364533dbc5f
Mesut <yildirimmesutyigit@gmail.com>
2023-05-17 21:25:15 +0300

    new commit

---
+++ javascript/class-notes/js07-strings/App.js
@@ -0,0 +1,47 @@
+// let str1 = "This is a string"
+// let str2 = 'This is a string'
+// let str3 = `This is a string`
+// let str4 = new String("This is a string")
+
+// // console.log(typeof str1) // string
+// // console.log(typeof str2) // string
+// console.log( str3) // string
+// console.log( str4) // object
+
+// console.log(str3[0])
+
+// const str = 'Hello World';
+
+// console.log(str.length)
+// console.log(str[6])
+// console.log(str[11])
+
+// for (let i=0; i<str.length; i++){
+//     console.log(str[i]);
+// }
+
+//tersten itere
+// for(let i = str.length -1   ; i>=0; i--){
+//     console.log(str[i])
+//   }
+
+// for(let i = str.length ; i>0; i--){
+//     console.log(str[i-1])
+//   }
+
+// let str1 = 'I\'m a string'
+// let str2 = "I'm a\0string\""
+// console.log(str2)
+
+// alert('1- ders \n2-Kayıt\n 3-pre-class')
+// alert(`1- ders
+// 2-Kayıt
+// 3-pre-class`)
+
+//slice
+
+// let str1 = "Clarusway";
+// let str2 = str1.slice(-3);
+// console.log(str1)
+// console.log(str2)
+// console.log(str1.slice(-9,-3))
