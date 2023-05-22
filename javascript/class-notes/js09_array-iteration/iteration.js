1000a78498797b6b665c4006adaa807088617096
Mesut <yildirimmesutyigit@gmail.com>
2023-05-22 21:50:58 +0300

    new commit

--- javascript/class-notes/js09_array-iteration/iteration.js
+++ javascript/class-notes/js09_array-iteration/iteration.js
@@ -3,3 +3,18 @@
 //* =======================================
 
 console.log("****** ITERATION **********");
+
+const prices = [250, 150, 300, 500];
+
+//* KLASIK FOR
+for (let i = 0; i < prices.length; i++) {
+  console.log(prices[i]);
+}
+console.log("*********");
+
+//* FOREACH
+prices.forEach((p) => console.log(p));
+
+console.log("*********");
+
+prices.forEach((p, i) => console.log("DEGER:", p, "INDIS:", i));
