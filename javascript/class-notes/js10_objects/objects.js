4614f41e49580cfa51ccd38ed3bb12603705bfa6
Mesut <yildirimmesutyigit@gmail.com>
2023-05-24 21:59:29 +0300

    new commit

--- javascript/class-notes/js10_objects/objects.js
+++ javascript/class-notes/js10_objects/objects.js
@@ -9,4 +9,30 @@
 //? Object'lerde Key-value (property-value) yapisi kullanilir.
 //? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
 
-console.log("******* Objects *********")
+console.log("******* Objects *********");
+
+const car = {
+  brand: "Mercedes",
+  model: 2020,
+  speed: 5,
+  engine: 2000,
+  color: ["red", "black", "white", "blue"],
+};
+console.log(car);
+
+console.log(car.brand);
+console.log(car.color[0]);
+car.color.forEach((c) => console.log(c));
+console.log(car.Engine);
+
+car.speed = 7;
+car.color = "purple";
+console.log(car);
+
+console.log("ENGINE:", car["engine"]);
+
+const myKey = prompt(
+  "Araba ile ilgili neyi ogrenmek istersiniz?: speed, color, engine..."
+);
+
+alert(car[myKey]);
