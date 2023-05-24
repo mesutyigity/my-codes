ff9faca37d10061a6cdea3236fcc2c243f7dc358
Mesut <yildirimmesutyigit@gmail.com>
2023-05-24 22:02:07 +0300

    new commit

--- javascript/class-notes/js10_objects/objects.js
+++ javascript/class-notes/js10_objects/objects.js
@@ -11,29 +11,62 @@
 
 console.log("******* Objects *********");
 
+//! 3 farkli yontem ile Object olusturulabilir
+
+//* ---------------------------------------------------------
+//* 1- Object literal (En cok tercih edilen yontem)
+//* ---------------------------------------------------------
+
 const car = {
   brand: "Mercedes",
   model: 2020,
-  speed: 5,
   engine: 2000,
-  color: ["red", "black", "white", "blue"],
+  speed: 5,
+  color: ["pink", "black", "blue", "green"],
 };
 console.log(car);
 
-console.log(car.brand);
-console.log(car.color[0]);
-car.color.forEach((c) => console.log(c));
-console.log(car.Engine);
+//? Objenin okunmasi (Read)
+console.log(car.brand); //* 1) . notasyonu ile erişim.
+console.log(car.color[0]); //? pink
 
+console.log(car.color);
+car.color.forEach((c) => console.log(c));
+
+console.log(car.Engine); //? undefined
+
+//? Objenin degerinin degisitirilmesi (write)
 car.speed = 7;
-car.color = "purple";
+car.color = "Morcivert";
+car.class = "E";
+
 console.log(car);
 
-console.log("ENGINE:", car["engine"]);
+//!  Uncaught TypeError: Assignment to constant variable.
+// car = {
+//   deneme: "1",
+// }
 
-const myKey = prompt(
-  "Araba ile ilgili neyi ogrenmek istersiniz?: speed, color, engine..."
-);
+//* 2) Square Bracket yontemi ile erisim
+//! bu yontem key isimlerinin degiskenlerde saklanmasına izin verdigi icin dinamik olarak
+// obje erisimlerinde kullanılabilir.
 
-alert(car[myKey]);
+console.log("ENGINE:", car["engine"]); //? objeAdi["keyAdi"]
 
+// const myKey = prompt(
+//   "Araba ile ilgili Neyi ogrenmek istersiniz: speed, color,engine ..."
+// )
+
+// alert(car[myKey])
+
+//* ---------------------------------------------------------
+//* 2- Object() class'ından new Operatörü ile
+//* ---------------------------------------------------------
+
+const myCar = new Object();
+myCar.brand = "Mercedes";
+myCar.model = 2022;
+myCar.speed = 7;
+
+console.log(myCar.speed);
+console.log(myCar["model"]);
