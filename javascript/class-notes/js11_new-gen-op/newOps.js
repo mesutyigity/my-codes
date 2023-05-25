db9206610f068c7c48b8ec0b0813579d4e102d15
Mesut <yildirimmesutyigit@gmail.com>
2023-05-25 21:56:29 +0300

    new commit

--- javascript/class-notes/js11_new-gen-op/newOps.js
+++ javascript/class-notes/js11_new-gen-op/newOps.js
@@ -74,3 +74,119 @@ const team = [
     age: 40,
   },
 ];
+//? KLASIK
+team.forEach((t) => {
+  console.log("NAME:", t.name);
+  console.log("SURNAME:", t.surname);
+  console.log("JOB:", t["job"]);
+  console.log("AGE:", t["age"]);
+  console.log("**************");
+});
+
+//? DESTR.
+team.forEach((person) => {
+  const { name, surname, job, age } = person;
+  console.log("NAME:", name);
+  console.log("SURNAME:", surname);
+  console.log("JOB:", job);
+  console.log("AGE:", age);
+  console.log("**************");
+});
+//!----  FUNCTIONLARDA DEATRUC. KULLANIMI ----
+
+//? Donus degeri olarak
+const getProduct = () => {
+  return {
+    id: "123",
+    pName: "NIKE",
+    price: 300,
+    stock: 1000,
+  };
+};
+
+// console.log(getProduct())
+let { pName, price, stock } = getProduct();
+//* function'un dondurdugu obje dogrudan dest. yapilabilir
+
+console.log("PRODUCT NAME:", pName, "PRICE:", price);
+
+stock = stock - 100;
+console.log("REMAINING STOCK:", stock);
+
+//? Parametre olarak
+const data = {
+  id: "123",
+  pName: "NIKE",
+  price: 300,
+};
+// const print = function (data) {
+//   const { id, pName, price } = data
+//   console.log(pName, price)
+// }
+
+const print = function ({ pName, price }) {
+  //? Yolda destr.
+  console.log(pName, price);
+};
+
+print(data);
+
+//* ======================================================
+//*  DESTRUCTURING (ARRAY)
+//* ======================================================
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffet"];
+
+//? Klasik
+const ismet = names[2];
+console.log(ismet);
+
+const [ahmo, , ismo, safo] = names;
+
+console.log(ahmo, ismo, safo);
+
+//*======================================================
+//*  REST (...)
+//* ======================================================
+
+//? REST operatoru kullanici tarafindan girilen degerleri dizi
+//? icerisine konumlandirir. Cesitli kullanim alanlari vardir.
+
+//! 1- Bir dizi veya object'deki bazi degerlerden geri kalanlarini
+//!    ayri dizi yada objelere kopyalanmasini saglayabilir.
+
+//* REST: Array
+const araclar = ["bmw", "merso", "ferarri", "rover", "lambo"];
+
+const [araba1, araba2, araba3, ...geriKalanlar] = araclar;
+console.log(araba1);
+console.log(geriKalanlar);
+
+//* REST: Object
+
+const kisi = {
+  ad: "Canan",
+  soyad: "Cano",
+  yas: 33,
+  meslek: "tester",
+};
+
+const { meslek, yas, ...isim } = kisi;
+
+console.log(meslek, yas);
+console.log(isim);
+console.log(kisi);
+
+//! 2- Bir fonksiyonun argumanlarini diziye cevirmek icin kullanilabilir.
+
+const topla = (a, b) => a + b;
+
+console.log("SONUC:", topla(2, 5, 10, 4));
+
+//? REST (...) ile non-iterable olan sayilari iterable hale (diziye)
+//? cevirmiş olduk.
+
+const toplaminiAl = (...sayilar) => {
+  console.log(sayilar);
+  return sayilar.reduce((t, v) => t + v, 0);
+};
+console.log("SONUC:", toplaminiAl(2, 5, 10, 4, 6, 8));
