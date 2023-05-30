4a4870753c338c1d599abaa195e5dcb070e8c07e
Mesut <yildirimmesutyigit@gmail.com>
2023-05-30 21:15:48 +0300

    new commit

--- javascript/class-notes/js12_dom-intro/selectors.js
+++ javascript/class-notes/js12_dom-intro/selectors.js
@@ -1,9 +1,16 @@
-console.log("***** SELECTORS *****")
+console.log("***** SELECTORS *****");
+
+document.title = "DOM DOM 🚀";
 
 //*===========================================
 //*            GETELEMENTBYID()
 //*===========================================
+// document.getElementById("header").style.border = "1px solid red"
+const myHeader = document.getElementById("header");
+console.log(myHeader);
 
+myHeader.style.backgroundColor = "red";
+myHeader.style.color = "white";
 //*===========================================
 //*          GETELEMENTSBYTAGNAME()
 //*===========================================
